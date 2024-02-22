758072b873e1dcaa6a538956b4aa3d813e5092bc
Bennai_Rayan <51482869+Rayan-mezben@users.noreply.github.com>
2024-02-22 10:38:15 +0100

    LocationRepository added

--- src/main/java/com/akatsuki/gpsapp/repository/LocationRepository.java
+++ src/main/java/com/akatsuki/gpsapp/repository/LocationRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 public interface LocationRepository extends CrudRepository<LocationEntity, Long> {
 
-    List<LocationResponseDto> getLocationEntitiesByTokens(List<TokenEntity> tokens);
+
 }
